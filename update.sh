2a7e0ab1ff195f23b53a01ab922e3e3c82062416
Pope_Rigby <poperigby@mailbox.org>
2023-12-17 14:02:15 -0800

    Use var for haxe version in update.sh

--- update.sh
+++ update.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+HAXE_VERSION=4.3.3
+
 echo -e "\033[32mGenerating haxe_sources.json file...\033[0m"
-opam pin add -k git -n haxe https://github.com/HaxeFoundation/haxe#4.3.3
+opam pin add -k git -n haxe https://github.com/HaxeFoundation/haxe#${HAXE_VERSION}
 opam tree --json=/tmp/haxe_deps.json haxe &> /dev/null
 ./flatpak-opam-generator/flatpak-opam-generator.py /tmp/haxe_deps.json > haxe_sources.json
 
